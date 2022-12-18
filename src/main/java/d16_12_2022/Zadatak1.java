697ea6b744b7fb72c1efdbc2f8180d4f537bf5a2
Katarina Deljanin <deljaaakat@gmail.com>
2022-12-18 12:05:31 +0100

    Domaci 16.12.2022.

---
+++ src/main/java/d16_12_2022/Zadatak1.java
@@ -0,0 +1,16 @@
+package d16_12_2022;
+
+import java.util.Scanner;
+
+public class Zadatak1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Unesite x: ");
+        int x = s.nextInt();
+
+        for (int i = 1; i < 10 ; i++) {
+            System.out.println(x+" x " + i + " = " + x*i);
+
+        }
+    }
+}
---
+++ src/main/java/d16_12_2022/Zadatak2.java
@@ -0,0 +1,13 @@
+package d16_12_2022;
+
+public class Zadatak2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 25; i++) {
+            if (i==5 || i==10 || i==15 || i==20) {
+                System.out.println();
+            }
+            System.out.print("_");
+            
+        }
+    }
+}
